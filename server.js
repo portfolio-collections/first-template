15d9c92941ace77ab7539793934be5ae15e61246
mr.eshtaiwi <mr.eshtaiwi@gmail.com>
2019-12-19 22:55:11 +0200

    fix issue

--- server.js
+++ server.js
@@ -3,15 +3,11 @@
 const express = require('express');
 const server = express();
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5503;
 // server.listen( PORT , () => {
 //   console.log('listening to port ' , PORT);
 // // });
 // server.get('/test', (request, response) => {
 //   response.send('working');
 // });
-server.listen(PORT,() => {
-  // eslint-disable-next-line no-console
-  console.log('listen');
-});
-server.use(express.static('/public'));
+server.use(express.static('public'));
