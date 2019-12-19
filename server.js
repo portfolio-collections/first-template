9c2b57d04afa129413d85915ec82a4bd282ca3cc
mr.eshtaiwi <mr.eshtaiwi@gmail.com>
2019-12-19 21:04:35 +0200

    add server to my site

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const server = express();
+const PORT = 5500;
+// server.listen( PORT , () => {
+//   console.log('listening to port ' , PORT);
+// });
+// server.get('/test', (request, response) => {
+//   response.send('working');
+// });
+// server.listen(PORT,() => {
+// console.log('listen');
+// });
+
+server.use(express.static('./public'));
